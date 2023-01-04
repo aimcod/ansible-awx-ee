b9a4048f361c8abb9472baa4d167ff1e46b6e57f
Andrei Mihai Iulian <andrei@aimcorp.co.uk>
2023-01-04 15:40:47 +0100

    Create doBuild.sh

---
+++ doBuild.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+ansible-runner worker --private-data-dir=/runner
+(builder) [chnladmin@dev-awx-ee-01 ansible-builder]$ cat doBuild.sh
+if [ -z $1 ]
+then
+        echo please provide the build number.
+
+        exit 1
+
+elif [ $1 -ge 9 ]
+        then
+                echo you\'ve reached maximum small version build numbers. please adjust.
+
+        else
+
+                python -m venv builder
+
+                source builder/bin/activate
+
+                ansible-builder build --tag quay.io/andreichnla/awx-ee:1.0.$1 --tag quay.io/andreichnla/awx-ee:latest --context ./ --verbosity 3
+                podman login -u="xxxxxxx" -p="xxxxxxxxxxxxxx" quay.io
+                podman push quay.io/andreichnla/awx-ee:1.0.$1
+                podman push quay.io/andreichnla/awx-ee:latest
+
+fi
